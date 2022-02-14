e32e4a21db6cffb6c94b67471924846d46c6760c
Aleandro2119 <aleandro.basilico@gmail.com>
2022-02-14 15:32:41 +0100

    Import axios

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,16 @@
 </template>
 
 <script>
+import axios from "axios";
 import Header from "./components/Header.vue";
 
 export default {
   name: "App",
   components: { Header },
+
+  mounted() {
+    axios.get("https://api.themoviedb.org/3/movie").then(() => {});
+  },
 };
 </script>
 
