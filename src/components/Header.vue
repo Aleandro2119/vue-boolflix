a32eeb68d58f20a42d2872c91198b5856d1e0635
Aleandro2119 <aleandro.basilico@gmail.com>
2022-02-14 15:33:07 +0100

    Add searchbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,23 @@
 <template>
   <header>
-    <div>cadakad</div>
+    <div>
+      <div>
+        <input v-model="search" type="text" /><button type="button">
+          Cerca
+        </button>
+      </div>
+    </div>
   </header>
 </template>
 
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      search: "",
+    };
+  },
 };
 </script>
 
