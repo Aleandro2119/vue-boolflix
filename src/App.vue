71f37d9552fd47aa40dd83f624b0e9480f8520bd
Aleandro2119 <aleandro.basilico@gmail.com>
2022-02-15 16:19:40 +0100

    Import axios and methods

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,49 @@
 <template>
   <div id="app">
-    <Header />
+    <Header @search="searchElement" />
+    <Main :films="films" />
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
 
 export default {
   name: "App",
-  components: { Header },
+  components: {
+    Header,
+    Main,
+  },
 
-  mounted() {
-    axios.get("https://api.themoviedb.org/3/movie").then(() => {});
+  data() {
+    return {
+      films: [],
+      api: {
+        language: "it-IT",
+        api_key: "c98fc689e4d66d8a1762d048d057e91b",
+        url: "https://api.themoviedb.org/3",
+      },
+    };
+  },
+
+  methods: {
+    searchElement(query) {
+      const { api_key, url, language } = this.api;
+
+      const config = {
+        params: {
+          api_key: api_key,
+          query: query,
+          language,
+        },
+      };
+
+      axios.get(`${url}/search/movie`, config).then((res) => {
+        this.films = res.data.results;
+      });
+    },
   },
 };
 </script>
